42ad147a2e4fc8ce6cb07ec7602b29a1d146e13d
NIDCHANAN <71490539+NIDCHANAN@users.noreply.github.com>
2024-07-06 23:08:53 +0700

    FirstCommit

---
+++ LanguageItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace CoreAdmin
+{
+    public class LanguageItem
+    {
+        public LanguageItem()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"{CultureCode} - {Name}";
+        }
+        public CultureInfo GetCulture()
+        {
+            return new CultureInfo(CultureCode);
+        }
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public bool IsRTL { get; set; }
+        public string FlagIcon { get; set; }
+        public string CultureCode { get; set; }
+    }
+}
---
+++ MyCustomRequestCultureProvider.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreAdmin
+{
+    public class MyCustomRequestCultureProvider : RequestCultureProvider
+    {
+        public override async Task<ProviderCultureResult> DetermineProviderCultureResult(HttpContext httpContext)
+        {
+            await Task.Yield();
+            return new ProviderCultureResult(Session.Language.CultureCode);
+        }
+    }
+}
